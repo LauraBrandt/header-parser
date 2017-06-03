e66391e080ad446282a7696db42f15d448812ec4
Laura Brandt <laurambrandt@gmail.com>
2017-06-03 21:25:05 +0000

    basic program written

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var port = process.env.PORT || 8080;
+
+app.get('/api/whoami', function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  var lang = req.headers['accept-language'].split(',')[0];
+  var os = req.headers['user-agent'].match(/\((.*?)\)/)[1];
+  
+  var final = {'IP Address' : ip,
+                'Language' : lang,
+                'Operating System' : os
+  };
+
+  res.end(JSON.stringify(final));
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(port, function () {
+  console.log('App listening on port', port);
 });
